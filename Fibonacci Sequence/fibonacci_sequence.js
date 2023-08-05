caf4104713d8b9266a3d62bbe34ff56f8ce8f10c
Jeph Mari Daligdig <122143538+greatxrider@users.noreply.github.com>
2023-08-05 10:37:54 +0800

    Add files via upload

---
+++ Fibonacci Sequence/fibonacci_sequence.js
@@ -0,0 +1,20 @@
+/* Fibonacci Sequence by Jeph Mari Daligdig*/
+
+function fibonacci(n) {
+    var output = [];
+    if (n === 1) {
+        output = [0];
+    }
+    else if (n === 2) {
+        output = [0, 1];
+    }
+    else{
+        output = [0, 1];
+        for (var i = 2; i < n; i++) {
+            output.push(output[output.length - 2] + output[output.length - 1]);
+        }
+    }
+    return output;
+}
+
+console.log(fibonacci(10));
